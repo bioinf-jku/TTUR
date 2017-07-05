f5275969fe20b5f83520dfb4ca77667ab6a3a8fe
Martin Heusel <mheusel@gmail.com>
2017-07-05 07:58:23 +0200

    removed = assignments

--- BEGAN_FID_batched/sh/run.sh
+++ BEGAN_FID_batched/sh/run.sh
@@ -4,18 +4,18 @@ update_k=True
 k_constant=0.08
 dataset=celebA_cropped
 #dataset=lsun_cropped
-train_stats_file="stats/stat_celeb_batched.pkl.gz"
+train_stats_file="stats/stat_celebA_batched.pkl.gz"
 lambda_k=0.001
 gamma=0.5
 python3 main_fid.py \
 --dataset $dataset \
 --train_stats_file $train_stats_file \
---input_height=64 \
---output_height=64 \
+--input_height 64 \
+--output_height 64 \
 --is_crop False \
---is_train=True \
---batch_size=16 \
---log_dir="logs" \
+--is_train True \
+--batch_size 16 \
+--log_dir "logs" \
 --d_lr $lr_d \
 --g_lr $lr_g \
 --lr_update_step 100000 \
