3a4ef0a1784d4b8954de7e442fd9e63c7d9b10e0
Thomas Unterthiner <unterthiner@bioinf.jku.at>
2017-08-23 14:21:37 +0200

    Import Inception net into a named scope

--- fid.py
+++ fid.py
@@ -39,13 +39,13 @@ def create_incpetion_graph(pth):
     with tf.gfile.FastGFile( pth, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString( f.read())
-        _ = tf.import_graph_def( graph_def, name='')
+        _ = tf.import_graph_def( graph_def, name='FID_Inception_Net')
 #-------------------------------------------------------------------------------
 
 
 def _get_inception_layer(sess):
     """Prepares inception net for batched usage and returns pool_3 layer. """
-    layername = 'pool_3:0'
+    layername = 'FID_Inception_Net/pool_3:0'
     pool3 = sess.graph.get_tensor_by_name(layername)
     ops = pool3.graph.get_operations()
     for op_idx, op in enumerate(ops):
@@ -93,7 +93,7 @@ def get_activations(images, sess, batch_size=50, verbose=False):
         start = i*batch_size
         end = start + batch_size
         batch = images[start:end]
-        pred = sess.run(inception_layer, {'ExpandDims:0': batch})
+        pred = sess.run(inception_layer, {'FID_Inception_Net/ExpandDims:0': batch})
         pred_arr[start:end] = pred.reshape(batch_size,-1)
     if verbose:
         print(" done")
