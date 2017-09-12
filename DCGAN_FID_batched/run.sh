c7d09e006cfa1c47dc76ccb4768b511a396f989d
Martin Heusel <mheusel@gmail.com>
2017-09-12 14:46:44 +0200

    fid_eval_steps

--- DCGAN_FID_batched/run.sh
+++ DCGAN_FID_batched/run.sh
@@ -1,16 +1,18 @@
 incept_path="inception-2015-12-05/classify_image_graph_def.pb"
+#dataset="lsun"
 dataset="celebA"
+#data_path="data/lsun_cropped"
 data_path="data/celebA_cropped"
 stats_path="stats/fid_stats_celeba.npz"
 lr_d=$1
 lr_g=$2
-load_checkpoint=false
 counter_start=0
+load_checkpoint=false
 if ! $load_checkpoint; then
   dwt=`date "+%m%d_%H%M%S"`
   run_id=${dwt}_${lr_d}_${lr_g}
 else
-  run_id="yydd_hhmmss_lr_d_lr_g"
+  run_id="ddMM_hhmmss_lrd_lrg"
 fi
 python3 main.py \
 --dataset=$dataset \
@@ -25,10 +27,11 @@ python3 main.py \
 --fid_n_samples 50000 \
 --fid_sample_batchsize 1000 \
 --fid_batch_size 100 \
+--fid_eval_steps 5000 \
 --learning_rate_d $lr_d \
 --learning_rate_g $lr_g \
 --beta1 0.5 \
---epoch 5000 \
+--epoch 81 \
 --load_checkpoint $load_checkpoint \
 --counter_start $counter_start \
 --incept_path $incept_path \
