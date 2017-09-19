9d0a8db07f36114009ff8e58bee49a21c626ee14
Martin Heusel <mheusel@gmail.com>
2017-09-19 11:50:57 +0200

    typo and some progress prints

--- precalc_stats_example.py
+++ precalc_stats_example.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import absolute_import, division, print_function
 
 import os
 import glob
@@ -12,22 +11,30 @@ import tensorflow as tf
 ########
 # PATHS
 ########
-data_path = '/local00/bioinf/celebA_cropped/' # set path to training set images
-output_path = '/local00/bioinf/tom/fid_stats_celeba.npz' # path for where to store the statistics
+data_path = 'data' # set path to training set images
+output_path = 'fid_stats.npz' # path for where to store the statistics
 # if you have downloaded and extracted
 #   http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
 # set this path to the directory where the extracted files are, otherwise
 # just set it to None and the script will later download the files for you
 inception_path = None
+print("check for inception model..", end=" ", flush=True)
 inception_path = fid.check_or_download_inception(inception_path) # download inception if necessary
-
+print("ok")
 
 # loads all images into memory (this might require a lot of RAM!)
+print("load images..", end=" " , flush=True)
 image_list = glob.glob(os.path.join(data_path, '*.jpg'))
 images = np.array([imread(str(fn)).astype(np.float32) for fn in image_list])
+print("%d images found and loaded" % len(images))
 
-fid.create_incpetion_graph(inception_path)  # load the graph into the current TF graph
+print("create inception graph..", end=" ", flush=True)
+fid.create_inception_graph(inception_path)  # load the graph into the current TF graph
+print("ok")
+
+print("calculte FID stats..", end=" ", flush=True)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     mu, sigma = fid.calculate_activation_statistics(images, sess, batch_size=100)
     np.savez_compressed(output_path, mu=mu, sigma=sigma)
+print("finished")
