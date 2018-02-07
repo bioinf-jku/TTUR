88621e3481c6114cbd80cdec4335a892d2037268
Martin Heusel <mheusel@gmail.com>
2018-02-07 13:48:52 +0100

    More stable version of calculate_frechet_distance()
    thanks so Dougal J. Sutherland

--- fid.py
+++ fid.py
@@ -18,11 +18,11 @@ See --help to see further details.
 
 from __future__ import absolute_import, division, print_function
 import numpy as np
-import scipy as sp
 import os
 import gzip, pickle
 import tensorflow as tf
 from scipy.misc import imread
+from scipy import linalg
 import pathlib
 import urllib
 
@@ -101,32 +101,58 @@ def get_activations(images, sess, batch_size=50, verbose=False):
 #-------------------------------------------------------------------------------
 
 
-def calculate_frechet_distance(mu1, sigma1, mu2, sigma2):
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
     """Numpy implementation of the Frechet Distance.
     The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
     and X_2 ~ N(mu_2, C_2) is
             d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+            
+    Stable version by Dougal J. Sutherland.
 
     Params:
     -- mu1 : Numpy array containing the activations of the pool_3 layer of the
              inception net ( like returned by the function 'get_predictions')
+             for generated samples.
     -- mu2   : The sample mean over activations of the pool_3 layer, precalcualted
                on an representive data set.
+    -- sigma1: The covariance matrix over activations of the pool_3 layer for
+               generated samples.
     -- sigma2: The covariance matrix over activations of the pool_3 layer,
                precalcualted on an representive data set.
 
     Returns:
-    -- dist  : The Frechet Distance.
-
-    Raises:
-    -- InvalidFIDException if nan occures.
+    --   : The Frechet Distance.
     """
-    m = np.square(mu1 - mu2).sum()
-    s = sp.linalg.sqrtm(np.dot(sigma1, sigma2))
-    dist = m + np.trace(sigma1+sigma2 - 2*s)
-    if np.isnan(dist):
-        raise InvalidFIDException("nan occured in distance calculation.")
-    return dist
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, "Training and test mean vectors have different lengths"
+    assert sigma1.shape == sigma2.shape, "Training and test covariances have different dimensions"
+
+    diff = mu1 - mu2
+
+    # product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        warnings.warn(("fid calculation produces singular product; adding {} to diagonal of "
+                       "cov estimates").format(eps))
+        offset = np.eye(sigma1.shape1[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError("Imaginary component {}".format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
 #-------------------------------------------------------------------------------
 
 
